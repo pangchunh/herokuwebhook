024e7fd36085d83d1b00877f05869f5d16f13336
Chun <cpang26@my.bcit.ca>
2023-12-19 18:05:36 -0800

    debugging

--- index.js
+++ index.js
@@ -7,16 +7,23 @@ const app = express();
 const validSignature = async (req, secret) => {
   const body = JSON.stringify(req.body);
   const heroku_hmac = req.headers['Heroku-Webhook-Hmac-SHA256'];
+  console.log(`heroku_hmac: ${heroku_hmac}`)
   if (!heroku_hmac) return false;
   const hmac = crypto.createHmac('sha256', secret);
   hmac.update(body);
   const digest = hmac.digest('base64');
 
+  console.log(`digest: ${digest}`)
+  
   return digest === heroku_hmac;
 }
 
 app.post('/webhook', async (req,res) => {
   const secret = process.env.SECRET;
+
+  const body = JSON.stringify(req.body);
+  console.log(`Receiving body from Heroku: ${body}`);
+
   const valid = await validSignature(req, secret);
   if (!valid) {
     res.status(403).send('Invalid Signature');
